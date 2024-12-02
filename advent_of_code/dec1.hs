b473f1dc8644a9ab32185fc4c8a055d88948dfff
vancodemeetup <the.meetup.email.address@proton.me>
2024-12-01 20:23:27 -0800

    advent of code 2024 dec 1st

---
+++ advent_of_code/dec1.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Data.List
+import qualified Data.Map as M
+
+main :: IO ()
+main = do
+  input <- readFile "dec1.txt"
+
+  let [la, lb] = transpose
+                 $ (read <$>) <$> words <$> lines input
+      sla = sort la
+      slb = sort lb
+
+  -- part 1
+  print . sum $ zipWith ( \ a b -> abs (a - b) ) sla slb
+
+  -- was distracted by a movie, didn't notice part 2
+  let lmb = foldr (flip M.insert 0) M.empty la
+      lmb' = foldr ( \ b m -> M.insert (head b) (length b) m ) lmb $ group slb
+
+  -- part 2
+  print . sum $ map ( \ a -> a * lmb' M.! a) la
